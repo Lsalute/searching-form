380d172e6833f36e3c38687325f6a352213f32d6
re <dyeudsla@gmail.com>
2019-04-22 15:22:35 +0900

    modified SearchModel.js

--- src/models/SearchModel.js
+++ src/models/SearchModel.js
@@ -2,12 +2,12 @@ const data = [
   {
     id: 1,
     name: '[키친르쎌] 홈메이드 칠리소스 포크립 650g',
-    image: 'https://cdn.bmf.kr/_data/product/H1821/5a4ed4e8a6751cb1cc089535c000f331.jpg'
+    image: 'https://cdn.pixabay.com/photo/2016/07/19/06/14/lobsters-1527602_960_720.jpg'
   },
   {
     id: 2,
     name: '[키친르쎌] 이탈리아 파티 세트 3~4인분',
-    image: 'https://cdn.bmf.kr/_data/product/H503E/300d931e3b8252ed628b6a3c2f56936b.jpg'
+    image: 'http://veloro.net/data/editor/1810/2e6c95ba4176cc603d862924382c4015_1540866502_4106.jpg'
   }
 ]
 
